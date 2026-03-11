9fb09bf4aa40dc4586f26eeff950dc1cf62eb9fb
Ripunjai Shukla <ripunjai79@email.com>
2026-03-11 16:01:02 +0530

    pratice

--- pratice7.java
+++ pratice7.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Shape {
     public void area (){
         System.out.println("display area");
@@ -11,5 +13,6 @@ class Triangle extends Shape{
 class EquilaterialTriangle extends Triangle{
  public void area (int l,int h){
         System.out.println(1/2*l*h);
+        Scanner
  }
 }
