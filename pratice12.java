d56eca625b2c74aad1fd3bf2aa798e865a58d60e
Ripunjai Shukla <ripunjai79@email.com>
2026-03-12 10:26:58 +0530

    Example

---
+++ pratice12.java
@@ -0,0 +1,12 @@
+public class pratice12 {
+    public static void main(String[] args) {
+        String name1 = "RipunjaiShukla";
+        String name2 = "Ripunjai Shukla";
+
+        if(name1.compareTo(name2) == 0){
+            System.out.println("Both the strings are equal");
+        }else{
+            System.out.println("Both the strings are not equal");
+        }
+    }
+}
